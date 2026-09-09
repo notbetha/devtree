0d857d60618cb96ff984026206d867125bafc739
notbetha <bethapunto@gmail.com>
2026-09-08 22:03:19 -0300

    feat: formulario de registro basico

---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import User from './models/User'
+
+const router = Router()
+
+router.post('/auth/register', async(req, res) => {
+    const user = new User(req.body)
+
+    await user.save()
+})
+
+export default router   

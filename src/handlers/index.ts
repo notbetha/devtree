f726ff4d0cd1dcd7fef109096bbda6c5ae1e81dc
notbetha <bethapunto@gmail.com>
2026-09-17 20:26:34 -0300

    feat: Anidando metodos para validacion de campos

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,18 +1,38 @@
 import type { Request, Response } from "express"
+import { validationResult } from "express-validator"
+import slug from 'slug'
 import User from "../models/User"
 import { hashPassword } from "../utils/Auth"
+import { error } from "node:console"
 
 export const createAccount = async(req: Request, res: Response) => {
+
+    let errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    console.log(errors)
+
+    return
 
     const { email, password } = req.body
     const userExists = await User.findOne({email})
     if(userExists) {
-        const error = new Error('El Usuario ya esta registrado')
+        const error = new Error('Un usuario con ese email ya esta registrado')
+        return res.status(409).json({error : error.message})
+    }
+
+    const handle = slug(req.body.handle, '')
+    const handleExists = await User.findOne({handle})
+    if(handleExists) {
+        const error = new Error('Nombre de usuario no disponible')
         return res.status(409).json({error : error.message})
     }
 
     const user = new User(req.body)
     user.password = await hashPassword(password)
+    user.handle = handle
 
     await user.save()
 
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,22 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createAccount } from './handlers'
 
 const router = Router()
 
-router.post('/auth/register', createAccount )
+router.post('/auth/register',
+    body('handle')
+        .notEmpty()
+        .withMessage('El handle no puede ir vacio'),
+    body('name')
+        .notEmpty()
+        .withMessage('El nombre no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('Email no válido'),
+    body('password')
+        .isLength({min: 8})
+        .withMessage('El password es muy corto, mínimo 8 caracteres'),
+    createAccount )
 
 export default router   
